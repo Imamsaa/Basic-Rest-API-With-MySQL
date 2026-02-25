b0f20f390dd441e087069078e67c8c264f0c3068
Imamsaa <imamsafii2222022@gmail.com>
2026-02-25 12:50:05 +0700

    feat : Refactoring Environment

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Buat pool SEKALI di luar app/route
 const dbpool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'restapi',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10,
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
-import userRouter from "./routes/users.js";
 import middlewareLogsRequest from "./middleware/logs.js";
+import dotenv from "dotenv";
+dotenv.config();
+import userRouter from "./routes/users.js";
 
 const app = express();
 
@@ -8,6 +10,6 @@ app.use(middlewareLogsRequest);
 app.use(express.json());
 app.use("/users", userRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
