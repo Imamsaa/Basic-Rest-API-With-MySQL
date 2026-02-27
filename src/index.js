d60be76cb66515273c2d33b261ae07a723d6ac72
Imamsaa <imamsafii2222022@gmail.com>
2026-02-27 08:31:50 +0700

    feat : Implementasi Multer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import middlewareLogsRequest from "./middleware/logs.js";
 import dotenv from "dotenv";
 dotenv.config();
 import userRouter from "./routes/users.js";
+import upload from "./middleware/multer.js";
 
 const app = express();
 
@@ -10,6 +11,13 @@ app.use(middlewareLogsRequest);
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/assets", express.static("public/images"));
+app.post("/upload", upload.single("photo"), (req, res) => {
+  res.send("File uploaded successfully");
+});
+
+app.use((err, req, res, next) => {
+  res.json({ message: err.message });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
---
+++ src/middleware/multer.js
@@ -0,0 +1,18 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + '-' + file.originalname);
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 3 * 1024 * 1024 }, // Limit file size to 5MB
+});
+
+export default upload;
